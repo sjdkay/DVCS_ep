2fbcbce8007d0cc8330c91389079197f3a6f2166
sjdkay <sjdkay@gmail.com>
2026-06-08 15:01:41 -0400

    Added processing scripts. Need to swtich to SBATCH rather than direct swif2 submission

---
+++ Process_DVCS.sh
@@ -0,0 +1,89 @@
+#! /bin/bash
+
+### Stephen Kay, University of York
+### 08/06/26
+### stephen.kay@york.ac.uk
+### A script to create and submit analysis jobs to the JLab farm.
+### As is, takes 3 arguments, campaign, beam energy combination and setting/comment for filelist
+
+if [ ! -n "${DVCS_ep+x}" ]; then
+    echo "!!!! Warning !!!!"
+    echo "Environemnt variable DVCS_ep not set, source setup.sh or setup.csh first!"
+    echo "Exiting now!"
+    echo "!!!! Warning !!!!"
+    exit 1
+fi
+
+SimDir=${DVCS_ep} # Put in the path of your directory here (where your eic-shell is)
+echo "Running as ${USER}"
+echo "Assuming simulation directory - ${SimDir}"
+
+Campaign=$1 # First arg is the campaign version to run
+if [[ -z "$1" ]]; then
+    echo "I need a campaign version to run!"
+    echo "Please provide a campaign version to run as the first argument, this should map to an available file list."
+    echo "E.g. 26_04_1"
+    exit 2
+fi
+BeamE=$2 # Second argument is a beam energy combination
+if [[ -z "$2" ]]; then
+    echo "I need a beam energy combination to analyse!"
+    echo "Please provide a beam energy combination as the second argument, this should map to an available file list."
+    echo "E.g. 10x130"
+    exit 3
+fi
+Setting="${3:-}" # Assigns third argument if it was provided, set to blank if not
+
+# Construct file list from arguments and check it exists
+FileList="${DVCS_ep}/filelists/inputFileList_ePIC_${Campaign}_${BeamE}_${Setting}.list"
+if [ ! -f ${FileList} ]; then
+    echo "${FileList} not found!"
+    echo "Check path and input arguments carefully!"
+    exit 4
+fi
+
+echo "Processing - ${FileList}"
+echo "Will run as chunks of 10 files (hard coded for now!)"
+
+# Check number of lines in file
+NumLines=$(wc -l < "${FileList}")
+# Split into 10 line chunks, 3 digit numeric suffix used to identify chunks, 001, 002, etc
+split -l 10 -d -a 3 --additional-suffix=.list "${FileList}" "${FileList%.list}_"
+# Find number of chunks list was split into and assign to variable to iterate over
+NumChunks=$(ls -1 ${FileList%.list}_* 2>/dev/null | wc -l)
+
+Timestamp=$(date +'%d_%m_%Y')
+Workflow="ePIC_DVCS_Analysis_${USER}_${Timestamp}" # Change this as desired
+# Define a disk space request. Change depending upon your needs. 
+Disk_Space=1
+for (( i=0; i<$NumChunks; i++ )) # Process all chunks
+do
+    printf -v ChunkNum "%03d" "${i}" # Convert integer to a padded 3 digit integer
+    JOBNAME="DVCS_Analysis_${Campaign}_${BeamE}_${Setting}_${ChunkNum}"
+    # The line below is the "guts" of this script. This is the script or job we will actually run on the farm node. Change the pathing to your script as needed. Submit the args as needed.
+    COMMAND="${DVCS_ep}/Process_DVCS_Job.sh ${Campaign} ${BeamE} ${Setting}_${ChunkNum}"
+    echo "Submitting batch job"
+    eval "swif2 add-job ${Workflow} -name ${JOBNAME} -disk 1GB -ram 4GB -stdout /farm_out/${USER}/swif/${Workflow}/out/${JOBNAME}.out -stderr /farm_out/${USER}/swif/${Workflow}/err/${JOBNAME}.err ${COMMAND}"
+    echo " "
+done
+
+eval 'swif2 run ${Workflow}'
+
+exit 0
+
+# Old version making a batch job card and submitting
+#batch="${DVCS_ep}/${Campaign}_${BeamE}_${Setting}_FileList_Chunk_${ChunkNum}.txt"
+#echo "Running ${batch}"
+#cp /dev/null ${batch}
+#echo "PROJECT: eic" >> ${batch}
+#echo "TRACK: analysis" >> ${batch}    
+#batch="${DVCS_ep}/${Campaign}_${BeamE}_${Setting}_FileList_Chunk_${ChunkNum}.txt"
+#echo "Running ${batch}"
+#cp /dev/null ${batch}
+#echo "PROJECT: eic" >> ${batch}
+#echo "TRACK: analysis" >> ${batch}    
+#echo "MAIL: ${USER}@jlab.org" >> ${batch} # Modify as desired
+
+#eval "swif2 add-jsub ${Workflow} -script ${batch} 2>/dev/null" # Add our created job to the swif2 workflow
+#sleep 1
+#rm ${batch} # Remove the job script after submission
---
+++ Process_DVCS_Job.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+
+### Stephen Kay, University of York
+### 08/06/26
+### stephen.kay@york.ac.uk
+### A script to process DVCS file lists with provided arguments.
+### As is, takes 3 arguments, campaign, beam energy combination and setting/comment for filelist
+
+if [ ! -n "${DVCS_ep+x}" ]; then
+    echo "!!!! Warning !!!!"
+    echo "Environemnt variable DVCS_ep not set, source setup.sh or setup.csh first!"
+    echo "Exiting now!"
+    echo "!!!! Warning !!!!"
+    exit 1
+fi
+
+SimDir=${DVCS_ep} # Put in the path of your directory here (where your eic-shell is)
+echo "Running as ${USER}"
+echo "Assuming simulation directory - ${SimDir}"
+
+Campaign=$1 # First arg is the campaign version to run
+if [[ -z "$1" ]]; then
+    echo "I need a campaign version to run!"
+    echo "Please provide a campaign version to run as the first argument, this should map to an available file list."
+    echo "E.g. 26_04_1"
+    exit 2
+fi
+BeamE=$2 # Second argument is a beam energy combination
+if [[ -z "$2" ]]; then
+    echo "I need a beam energy combination to analyse!"
+    echo "Please provide a beam energy combination as the second argument, this should map to an available file list."
+    echo "E.g. 10x130"
+    exit 3
+fi
+Setting="${3:-}" # Assigns third argument if it was provided, set to blank if not
+
+# Construct file list from arguments and check it exists
+FileList="${DVCS_ep}/filelists/inputFileList_ePIC_${Campaign}_${BeamE}_${Setting}.list"
+if [ ! -f ${FileList} ]; then
+    echo "${FileList} not found!"
+    echo "Check path and input arguments carefully!"
+    exit 4
+fi
+
+echo "Processing - ${FileList}"
+
+export EICSHELL=/group/eic/users/${USER}/ePIC/eic-shell # Must point to where your eic-shell is!
+cat <<EOF | $EICSHELL
+cd ${DVCS_ep}
+source setup.sh
+root -l -b -q 'run_ePIC_DVCS.C("${Campaign}", "${BeamE}", "${Setting}")'
+EOF
+
+sleep 2
+
+rm $FileList
+
+exit 0
